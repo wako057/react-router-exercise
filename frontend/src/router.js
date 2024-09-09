c146735ae3bd7f75ef40295efa6f77e424b1d034
Gregory Gerard <wako057@gmail.com>
2024-09-09 17:28:45 +0200

    feat: add bonus nested routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -6,6 +6,7 @@ import EventsPage from "./pages/EventsPage";
 import EditEventPage from "./pages/EditEventPage";
 import NewEventPage from "./pages/NewEventPage";
 import EventDetailPage from "./pages/EventDetailPage";
+import EventLayout from "./pages/EventLayout";
 
 
 export const router = createBrowserRouter([
@@ -15,10 +16,16 @@ export const router = createBrowserRouter([
         errorElement: <ErrorPage />,
         children: [
             { index: true, element: <HomePage /> },
-            { path: 'events', element: <EventsPage /> },
-            { path: 'events/:eventId', element: <EventDetailPage /> },
-            { path: 'events/new', element: <NewEventPage /> },
-            { path: 'events/:eventId/edit', element: <EditEventPage /> },
+            {
+                path: 'events', element: <EventLayout />,
+                children: [
+                    { index: true, element: <EventsPage /> },
+                    { path: ':eventId', element: <EventDetailPage /> },
+                    { path: 'new', element: <NewEventPage /> },
+                    { path: ':eventId/edit', element: <EditEventPage /> },
+
+                ]
+            },
 
         ],
     }
